dbc258332185b730d3fa14ae63c0146c26af15f4
Johnathon Barhydt <jrbarhydt@gmail.com>
2020-04-21 01:28:03 -0700

    add docstrings

--- models/users.py
+++ models/users.py
@@ -1,38 +1,92 @@
-from mongoengine import Document, EmbeddedDocument, EmbeddedDocumentField, ListField, StringField, \
-    EmailField, DateField, ReferenceField, BooleanField
+from mongoengine import (Document,
+                         EmbeddedDocument,
+                         EmbeddedDocumentField,
+                         ListField,
+                         StringField,
+                         EmailField,
+                         BooleanField)
 from flask_bcrypt import generate_password_hash, check_password_hash
+from models.meals import Meals
 import re
 
 
 class Access(EmbeddedDocument):
-    guest = BooleanField(default=True)
+    """
+    Custom EmbeddedDocument to set user authorizations.
+
+    :param user: boolean value to signify if user is a user
+    :param admin: boolean value to signify if user is an admin
+    """
     user = BooleanField(default=True)
-    server = BooleanField(default=False)
-    cook = BooleanField(default=False)
-    manager = BooleanField(default=False)
     admin = BooleanField(default=False)
 
 
 class PhoneField(StringField):
+    """
+    Custom StringField to verify Phone numbers.
+
     # Modification of http://regexlib.com/REDetails.aspx?regexp_id=61
     #
     # US Phone number that accept a dot, a space, a dash, a forward slash, between the numbers.
     # Will Accept a 1 or 0 in front. Area Code not necessary
+    """
     REGEX = re.compile(r"((\(\d{3}\)?)|(\d{3}))([-\s./]?)(\d{3})([-\s./]?)(\d{4})")
 
     def validate(self, value):
+        # Overwrite StringField validate method to include regex phone number check.
         if not PhoneField.REGEX.match(string=value):
             self.error(f"ERROR: `{value}` Is An Invalid Phone Number.")
         super(PhoneField, self).validate(value=value)
 
 
-class User(Document):
+class Users(Document):
+    """
+    Template for a mongoengine document, which represents a user.
+    Password is automatically hashed before saving.
+
+    :param email: unique required email-string value
+    :param password: required string value, longer than 6 characters
+    :param access: Access object
+    :param fav_meals: List of Meal objects
+    :param name: option unique string username
+    :param phone: optional string phone-number, must be valid via regex
+    :Example:
+
+    >>> import mongoengine
+    >>> from app import default_config
+
+    >>> mongoengine.connect(**default_config['MONGODB_SETTINGS'])
+    MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True, read_preference=Primary())
+
+    # Create test user
+    >>> new_user = Users(email="spam@ham-and-eggs.com", password="hunter2", access={"admin": True})
+    >>> new_user.save()
+    >>> new_user.name = "spammy"
+    >>> new_user.save()
+
+    # Remove test user
+    >>> new_user.delete()
+
+    .. seealso:: :class:`Access`, :class:`Phone`, :class:`models.meals.Meals`
+    """
     email = EmailField(required=True, unique=True)
     password = StringField(required=True, min_length=6, regex=None)
-    access = ListField(EmbeddedDocumentField(Access), default=[Access(guest=True)])
+    access = EmbeddedDocumentField(Access, default=Access(user=True, admin=False))
+    fav_meals = ListField(Meals)
+    name = StringField(unique=True)
+    phone = PhoneField()
 
     def generate_pw_hash(self):
         self.password = generate_password_hash(password=self.password).decode('utf-8')
+    # Use documentation from BCrypt for password hashing
+    generate_pw_hash.__doc__ = generate_password_hash.__doc__
 
     def check_pw_hash(self, password):
         return check_password_hash(pw_hash=self.password, password=password)
+    # Use documentation from BCrypt for password hashing
+    check_pw_hash.__doc__ = check_password_hash.__doc__
+
+    def save(self, *args, **kwargs):
+        # Overwrite Document save method to generate password hash prior to saving
+        self.generate_pw_hash()
+        super(Users, self).save(*args, **kwargs)
